38fed423a259ea4cf21ab3432431e1cb86ea8e62
Arne Brasseur <arne@arnebrasseur.net>
2015-11-21 19:58:15 +0100

    Initial success with midi and beads

--- project.clj
+++ project.clj
@@ -1,6 +1,22 @@
 (defproject beadicious "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]])
+  :license {:name "Mozilla Public License Version 2.0"
+            :url "https://www.mozilla.org/en-US/MPL/2.0/"}
+
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [org.clojure/core.async "0.2.374"]]
+
+  :resource-paths ["beads-jars/org-jaudiolibs-audioservers.jar"
+                   "beads-jars/jl1.0.1.jar"
+                   "beads-jars/org-jaudiolibs-audioservers-jack.jar"
+                   "beads-jars/beads.jar"
+                   "beads-jars/jna.jar"
+                   "beads-jars/tritonus_share.jar"
+                   "beads-jars/org-jaudiolibs-audioservers-javasound.jar"
+                   "beads-jars/tools.jar"
+                   "beads-jars/beads-io.jar"
+                   "beads-jars/jarjar-1.0.jar"
+                   "beads-jars/tritonus_aos-0.3.6.jar"
+                   "beads-jars/mp3spi1.9.4.jar"
+                   "beads-jars/org-jaudiolibs-jnajack.jar"])
--- src/beadicious/core.clj
+++ src/beadicious/core.clj
@@ -1,6 +1,143 @@
-(ns beadicious.core)
+(ns beadicious.core
+  (:import
+   [javax.sound.midi MidiDevice MidiSystem Receiver MidiMessage ShortMessage]
+   [net.beadsproject.beads.core AudioContext UGen]
+   [net.beadsproject.beads.data Buffer]
+   [net.beadsproject.beads.ugens WavePlayer Gain]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+
+(defmethod clojure.core/print-method MidiDevice
+  [device writer]
+  (.write writer (str (.getName (class device)) "<" (.getDeviceInfo device) ">")))
+
+;; AudioContext ac = new AudioContext();
+;; WavePlayer wp = new WavePlayer(ac, 440.0f, Buffer.SINE);
+;; ac.out.addInput(wp);
+;; ac.start();
+
+(defn query-midi-devices []
+  (map #(MidiSystem/getMidiDevice %)
+       (MidiSystem/getMidiDeviceInfo)))
+
+(defn find-midi-device [name]
+  (some #(and (.contains (str %) name) %) (query-midi-devices)))
+
+(def midi-commands
+  {ShortMessage/ACTIVE_SENSING         :activeSensing
+   ShortMessage/CHANNEL_PRESSURE       :channelPressure
+   ShortMessage/CONTINUE               :continue
+   ShortMessage/CONTROL_CHANGE         :controlChange
+   ShortMessage/END_OF_EXCLUSIVE       :endOfExclusive
+   ShortMessage/MIDI_TIME_CODE         :midiTimeCode
+   ShortMessage/NOTE_OFF               :noteOff
+   ShortMessage/NOTE_ON                :noteOn
+   ShortMessage/PITCH_BEND             :pitchBend
+   ShortMessage/POLY_PRESSURE          :polyPressure
+   ShortMessage/PROGRAM_CHANGE         :programChange
+   ShortMessage/SONG_POSITION_POINTER  :songPositionPointer
+   ShortMessage/SONG_SELECT            :songSelect
+   ShortMessage/START                  :start
+   ShortMessage/STOP                   :stop
+   ShortMessage/SYSTEM_RESET           :systemReset
+   ShortMessage/TIMING_CLOCK           :timingClock
+   ShortMessage/TUNE_REQUEST           :tuneRequest})
+
+(defn parse-midi-message [message]
+  (if (instance? ShortMessage message)
+    (let [command (get midi-commands (.getCommand message))
+          channel (.getChannel message)
+          data1   (.getData1 message)
+          data2   (.getData2 message)
+          result  {:command command :channel channel :data1 data1 :data2 data2}]
+      (merge result
+             (case command
+               (:noteOn :noteOff) {:note data1 :velocity data2}
+               (:controlChange)   {:control data1 :value data2}
+               )))))
+
+
+(defn midi-listen [device-name fn]
+  (let [device (find-midi-device device-name)
+        transmitter (.getTransmitter device)
+        receiver (proxy [Receiver] []
+                       (send [^MidiMessage message ^double timestamp]
+                         (if-let [msg (parse-midi-message message)]
+                           (fn msg timestamp))))]
+    (.open device)
+    (.setReceiver transmitter receiver)
+    [device transmitter]))
+
+(def device (midi-listen "MidiInDevice" #(println %1 %2)))
+
+(map #(.close %) device)
+
+
+;; (com.sun.media.sound.SoftSynthesizer<Gervill>
+;; com.sun.media.sound.MidiInDevice<Mini [hw:1,0,0]>
+;; com.sun.media.sound.MidiInDevice<Mini [hw:1,0,1]>
+;; com.sun.media.sound.MidiOutDevice<Mini [hw:1,0,0]>
+;; com.sun.media.sound.MidiOutDevice<Mini [hw:1,0,1]>
+;; com.sun.media.sound.RealTimeSequencer<Real Time Sequencer>)
+
+
+
+
+
+
+Buffer/SINE
+Buffer/SQUARE
+
+(def ac
+  (AudioContext.))
+
+(defn ->WavePlayer [^AudioContext ac ^double freq ^Buffer buffer]
+  (WavePlayer. ac freq buffer))
+
+(defn ->UgenWavePlayer [^AudioContext ac ^UGen ugen ^Buffer buffer]
+  (WavePlayer. ac ugen buffer))
+
+(.addInput (.out ac)
+           (->WavePlayer ac 440 Buffer/SINE))
+
+(.start ac)
+
+(.stop ac)
+
+;;  In this line of code, a Gain object is instantiated with one input at a starting volume of 0.2, or 20%.
+
+;; Gain g = new Gain(ac, 1, 0.2f);
+;; g.addInput(wp);
+;; ac.out.addInput(g);
+
+
+
+
+;; // set up the keyboard input
+;;     MidiKeyboard keys = new MidiKeyboard();
+;;     keys.addActionListener(new ActionListener(){
+;;       @Override
+;;       public void actionPerformed(ActionEvent e)
+;;       {
+;;         // if the event is not null
+;;         if( e != null )
+;;         {
+;;           // if the event is a MIDI event
+;;           if( e.getSource() instanceof ShortMessage )
+;;           {
+;;             // get the MIDI event
+;;             ShortMessage sm = (ShortMessage)e.getSource();
+
+;;             // if the event is a key down
+;;             if( sm.getCommand() == MidiKeyboard.NOTE_ON && sm.getData2() > 1 )
+;;             {
+;;               keyDown(sm.getData1());
+;;             }
+;;             // if the event is a key up
+;;             else if( sm.getCommand() == MidiKeyboard.NOTE_OFF )
+;;             {
+;;               keyUp(sm.getData1());
+;;             }
+;;           }
+;;         }
+;;       }
+;;     });
